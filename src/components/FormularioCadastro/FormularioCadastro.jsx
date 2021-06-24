ac8c0d05a05c367bd1fc5d298e8ec6a8e648c53d
Iuri Dias Ribeiro <iuri.dias968@gmail.com>
2021-06-24 17:08:32 -0300

    Aula 03.05 Organizando projeto

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
+import './estilo.css';
 
 class FormularioCadastro extends Component {
     state = {}
     render() {
         return (
-            <form>
-                <input type="text" placeholder="Título"></input>
-                <textarea placeholder="Escreve sua nota..." />
-                <button>Criar Nota</button>
+            <form className="form-cadastro">
+                <input type="text" placeholder="Título" className="form-cadastro_input"></input>
+                <textarea rows={15} placeholder="Escreve sua nota..." className="form-cadastro_input" />
+                <button className="form-cadastro_input form-cadastro_submit">Criar Nota</button>
             </form>
         );
     }
