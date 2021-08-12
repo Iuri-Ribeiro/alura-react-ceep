c191e3d3f2b3a953899a2c1d39e40bb7ee3dcf74
Iuri Dias Ribeiro <iuri.dias968@gmail.com>
2021-08-12 19:06:49 -0300

    Aula 01.04 Deletando Cards

--- src/App.js
+++ src/App.js
@@ -6,30 +6,38 @@ import './assets/App.css';
 
 class App extends Component {
 
-  constructor() {
-    super();
-    this.state = {
-      notas: []
-    };
-  }
-
-  criarNota(titulo, texto) {
-    const novaNota = { titulo, texto };
-    const novoArrayNotas = [...this.state.notas, novaNota]
-    const novoState = {
-      notas: novoArrayNotas
+    constructor() {
+        super();
+        this.state = {
+            notas: []
+        };
     }
-    this.setState(novoState);
-  }
 
-  render() {
-    return (
-      <section className="conteudo">
-        <FormularioCadastro criarNota={this.criarNota.bind(this)} />
-        <ListaDeNotas notas={this.state.notas} />
-      </section>
-    );
-  }
+    criarNota(titulo, texto) {
+        const novaNota = { titulo, texto };
+        const novoArrayNotas = [...this.state.notas, novaNota]
+        const novoState = {
+            notas: novoArrayNotas
+        }
+        this.setState(novoState);
+    }
+
+    deletarNota(index) {
+        let arrayDeNotas = this.state.notas;
+        arrayDeNotas.splice(index, 1);
+        this.setState({ notas: arrayDeNotas });
+    }
+
+    render() {
+        return (
+            <section className="conteudo">
+                <FormularioCadastro criarNota={this.criarNota.bind(this)} />
+                <ListaDeNotas
+                    notas={this.state.notas}
+                    deletarNota={this.deletarNota.bind(this)} />
+            </section>
+        );
+    }
 }
 
 export default App;
--- src/components/ListaDeNotas/ListaDeNotas.jsx
+++ src/components/ListaDeNotas/ListaDeNotas.jsx
@@ -9,7 +9,11 @@ class ListaDeNotas extends Component {
                 {this.props.notas.map((nota, index) => {
                     return (
                         <li className="lista-notas_item" key={index}>
-                            <CardNota titulo={nota.titulo} texto={nota.texto} />
+                            <CardNota
+                                titulo={nota.titulo}
+                                texto={nota.texto}
+                                indice={index}
+                                deletarNota={this.props.deletarNota.bind(this)} />
                         </li>
                     );
                 })}
